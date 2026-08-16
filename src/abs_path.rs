b827378f8c2b870b6a8ed3eb9ee94ced75647183
Schneems <richard.schneeman+foo@gmail.com>
2026-08-16 15:50:52 +0000

    Function

--- src/abs_path.rs
+++ src/abs_path.rs
@@ -73,6 +73,9 @@ impl AbsPath {
     /// the format is the same.
     ///
     /// Errors if path is not a directory or is not readable
+    pub fn join_relative(&self, path: &RelativePath) -> AbsPath {
+        AbsPath(self.as_ref().join(path.as_ref()))
+    }
     pub(crate) fn read_dir(&self) -> Result<Vec<AbsPath>, std::io::Error> {
         #[cfg_attr(not(test), allow(unused_mut))]
         let mut entries: Vec<AbsPath> = std::fs::read_dir(&self.0)?
